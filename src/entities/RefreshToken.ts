624bcb0fda21841071807815101c5a3d013a49e3
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2023-05-27 10:31:42 +0530

    added alt types and validation methods

--- src/entities/RefreshToken.ts
+++ src/entities/RefreshToken.ts
@@ -5,12 +5,12 @@ import User from "./User";
 @Entity("refresh_tokens")
 export default class RefreshToken extends BaseEntity {
 
-  // serves as jti claim for jet
+  // serves as jti claim for jwt
   @PrimaryGeneratedColumn("uuid")
   public id!: string;
 
   @ManyToOne(() => User, (user) => user.refreshTokens, { onDelete: "CASCADE" })
-  public user: User;
+  public user!: User | string;
 
   @OneToMany(() => AccessToken, (at) => at.refreshToken, { cascade: ['soft-remove'] })
   public accessTokens: AccessToken[];
@@ -19,9 +19,19 @@ export default class RefreshToken extends BaseEntity {
   public createdAt!: Date;
 
   @Column({ name: "expires_at", type: "timestamptz" })
-  public expiresAt!: Date;
+  public expiresAt!: Date | number | string;
 
   @DeleteDateColumn({ name: "deleted_at" })
   public deletedAt!: Date;
+
+  isValid(): boolean {
+    return this.deletedAt !== null && this.expiresAt < new Date();
+  }
+
+  needsRenewal(): boolean {
+    const now = new Date();
+    now.setDate(now.getDate() - 1);
+    return this.deletedAt !== null && this.expiresAt > now && this.expiresAt < new Date();
+  }
 
 }
